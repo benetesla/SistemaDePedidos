2ceedc02cd54d4e439a7039b4645399f5de81b2b
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-12 18:32:33 -0300

    Inicio do sistema de pedidos. Criacao do formulario de pedidos. Criacao da ORDER PHP. Inicio da estilizacao da pagina com css.

---
+++ order.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Sistema de Pedidos</title>
+    <link rel="stylesheet" href="./css/styles.css" type="text/css" />
+</head>
+
+<body>
+    <h1>Sistema de Pedidos</h1>
+    <h2> Resultados do Pedido </h2>
+    <p> Obrigado por fazer seu pedido. </p>
+    <?php echo "Seu pedido foi processado às " . date("H:i, jS F"); ?>
+</body>
+
+</html>
