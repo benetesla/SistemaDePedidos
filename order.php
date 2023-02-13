247b9a2155a1859b07aa7916d3f04dbc4d75306c
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-13 14:35:49 -0300

    Adicionando a details no resumo do meu pedido

--- order.php
+++ order.php
@@ -21,24 +21,20 @@
     echo htmlspecialchars($oilqty) . ' litros de óleo<br />';
     echo htmlspecialchars($sparkqty) . ' velas de ignição<br />';
     echo '</p>';
-    echo '<h3>Resumo do pedido';
+    echo '<div class="pedidoCard">Resumo do pedido <br />';
     $totalqty = 0;
     $totalqty = $tireqty + $oilqty + $sparkqty;
-    echo 'Items ordered: ' . $totalqty . '<br />';
+    echo '<details>Items do pedido: ' . $totalqty . '<br />';
     $totalamount = 0.00;
     define('TIREPRICE', 100);
     define('OILPRICE', 10);
-    define('SPARKPRICE', 4);
-   
-   
-    $totalamount = $tireqty * TIREPRICE
-        + $oilqty * OILPRICE
-        + $sparkqty * SPARKPRICE;
+    define('SPARKPRICE', 4);     
+    $totalamount = $tireqty * TIREPRICE + $oilqty * OILPRICE + $sparkqty * SPARKPRICE;
     echo 'Subtotal: $' . number_format($totalamount, 2) . '<br />';
     $taxrate = 0.10;
     $totalamount = $totalamount * (1 + $taxrate);
     echo 'Total incluindo impostos: $' . number_format($totalamount, 2) . '<br />';
-    echo '</h3>';
+    echo '</div>';
     ?>
 </body>
 
