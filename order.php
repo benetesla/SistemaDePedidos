c67793d42d70cd9726e5b62389ae365deb870159
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-13 09:30:42 -0300

    Imprimindo os primieros resultados do formulario.

--- order.php
+++ order.php
@@ -8,8 +8,34 @@
 <body>
     <h1>Sistema de Pedidos</h1>
     <h2> Resultados do Pedido </h2>
-    <p> Obrigado por fazer seu pedido. </p>
-    <?php echo "Seu pedido foi processado às " . date("H:i, jS F"); ?>
+
+    <?php
+    $tireqty = $_POST['tireqty'];
+    $oilqty = $_POST['oilqty'];
+    $sparkqty = $_POST['sparkqty'];
+    echo '<p>Ordem processada às ';
+    echo date('H:i, jS F Y');
+    echo '</p>';
+    echo '<p>Seu pedido é como segue: </p>';
+    echo htmlspecialchars($tireqty) . ' pneus<br />';
+    echo htmlspecialchars($oilqty) . ' litros de óleo<br />';
+    echo htmlspecialchars($sparkqty) . ' velas de ignição<br />';
+    $totalqty = 0;
+    $totalqty = $tireqty + $oilqty + $sparkqty;
+    echo 'Items ordered: ' . $totalqty . '<br />';
+    $totalamount = 0.00;
+    define('TIREPRICE', 100);
+    define('OILPRICE', 10);
+    define('SPARKPRICE', 4);
+    $totalamount = $tireqty * TIREPRICE
+        + $oilqty * OILPRICE
+        + $sparkqty * SPARKPRICE;
+    echo 'Subtotal: $' . number_format($totalamount, 2) . '<br />';
+    $taxrate = 0.10;  // local sales tax is 10%
+    $totalamount = $totalamount * (1 + $taxrate);
+    echo 'Total incluindo impostos: $' . number_format($totalamount, 2) . '<br />';
+    
+    ?>
 </body>
 
 </html>
