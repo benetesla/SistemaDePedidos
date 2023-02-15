bb4d904f89afa90524675dcb00187abf5e86eb03
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-15 11:26:06 -0300

    Ontem vi um video de um desenho antigo(Wheel squad) e decidi mudar um pouco esse projeto,criando uma loja para pedidos via php e mysql

---
+++ Shop/index.php
@@ -0,0 +1,33 @@
+<?php
+require_once("File/component.php");
+?>
+
+    <!DOCTYPE html>
+    <html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wheel Squad</title>
+        <link rel="shortcut icon" href="./upload/Wheel_Squad.jpg" type="image/x-icon">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="./Css/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row text-center py-5">
+               <?php
+                component("Street Rims", "R$170", "./upload/Produt1.jpg", 1);
+                component("Street Rims2", "R$190", "./upload/Produt1.jpg", 2);
+                component("Street Rims3", "R$180", "./upload/Produt1.jpg", 3);
+                component("Street Rims4", "R$197", "./upload/Produt1.jpg", 4);
+                ?>
+            </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
